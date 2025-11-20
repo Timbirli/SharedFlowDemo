ed4305dfd1e8da7bab5861f225766798514141f6
Timbirli <timbirli16@yandex.ru>
2025-11-20 22:13:29 +0700

    Greeting_delete+ScreenSetup and MainScreen создание

--- app/src/main/java/com/example/sharedflowdemo/MainActivity.kt
+++ app/src/main/java/com/example/sharedflowdemo/MainActivity.kt
@@ -20,8 +20,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             SharedFlowDemoTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    ScreenSetup(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,12 +30,13 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun ScreenSetup(modifier: Modifier = Modifier) {
+    MainScreen(modifier)
 }
+@Composable
+fun MainScreen(modifier: Modifier = Modifier) {
+}
+
 
 @Preview(showBackground = true)
 @Composable
